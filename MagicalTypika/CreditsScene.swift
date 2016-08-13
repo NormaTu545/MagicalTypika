1151e1232ce4698cfad7699e8bce4ba9b6c55d7a
NormaTu545 <NormaTu545@gmail.com>
2016-08-13 10:36:53 -0700

    [Credits] Temp info populated
    Will individually credit artists after demo day.

--- MagicalTypika/CreditsScene.swift
+++ MagicalTypika/CreditsScene.swift
@@ -12,6 +12,11 @@ import SpriteKit
 class CreditsScene: SKScene {
     
     var okButton: ButtonNode!
+    var firstLabel: SKLabelNode!
+    var secondLabel: SKLabelNode!
+    var thirdLabel: SKLabelNode!
+    var fourthLabel: SKLabelNode!
+
     var backgroundMusic: SKAudioNode!
     
     override func didMoveToView(view: SKView) {
@@ -24,6 +29,37 @@ class CreditsScene: SKScene {
         background.size = view.frame.size
         background.zPosition = -2
         
+        firstLabel = SKLabelNode(fontNamed: "Courier New Bold")
+        firstLabel.fontSize = 15
+        firstLabel.position.x = size.width/2
+        firstLabel.position.y = size.height - (size.height/8)
+        firstLabel.zPosition = 100
+        firstLabel.text = "Information to be updated soon!"
+        addChild(firstLabel)
+        
+        secondLabel = SKLabelNode(fontNamed: "Courier New Bold")
+        secondLabel.fontSize = 12
+        secondLabel.position.x = size.width/2
+        secondLabel.position.y = firstLabel.position.y - 50
+        secondLabel.zPosition = 100
+        secondLabel.text = "Sound from Newgrounds Audio & Freesound.org"
+        addChild(secondLabel)
+        
+        thirdLabel = SKLabelNode(fontNamed: "Courier New Bold")
+        thirdLabel.fontSize = 12
+        thirdLabel.position.x = size.width/2
+        thirdLabel.position.y = secondLabel.position.y - 50
+        thirdLabel.zPosition = 100
+        thirdLabel.text = "Sprite Art from MapleSimulator.com"
+        addChild(thirdLabel)
+        
+        fourthLabel = SKLabelNode(fontNamed: "Courier New Bold")
+        fourthLabel.fontSize = 12
+        fourthLabel.position.x = size.width/2
+        fourthLabel.position.y = thirdLabel.position.y - 50
+        fourthLabel.zPosition = 100
+        fourthLabel.text = "All Assets used under Creative Commons"
+        addChild(fourthLabel)
         
         //MARK: [OK Button] Brings us back to main menu
         okButton = ButtonNode(normalImageNamed: "okButton", activeImageNamed: "okButton", disabledImageNamed: "okButton")
